e9a34f77d63168094f41bb4751ffd10767e01baa
Charles Crete <charles@cretezy.com>
2025-04-18 18:21:37 -0400

    fix: Add +1 to line

--- godot-neovim.sh
+++ godot-neovim.sh
@@ -9,14 +9,15 @@ nvim_exec="nvim"
 
 server_path=".godot/server.pipe"
 filename=$(printf %q "$1") # Escaping
-cursor=$2
+line=$2
+col=$3
 
 start_server() {
-    "$term_exec" "$nvim_exec" --listen "$server_path" "+call cursor($cursor)" "$filename"
+    "$term_exec" "$nvim_exec" --listen "$server_path" "+call cursor($line+1,$col)" "$filename"
 }
 
 open_file_in_server() {
-    "$nvim_exec" --server "$server_path" --remote-send "<C-\><C-n>:e $filename<CR>:call cursor($cursor)<CR>"
+    "$nvim_exec" --server "$server_path" --remote-send "<C-\><C-n>:e $filename<CR>:call cursor($line+1,$col)<CR>"
 }
 
 # Check if server is currently running
